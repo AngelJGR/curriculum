395909527e2b65286af4f2e3d11d505d1f581e9a
AngelJGR <ajguerreror@gmail.com>
2020-12-07 15:14:16 -0400

    (client): Añade validadores al formulario del login

--- client/src/views/options/login.vue
+++ client/src/views/options/login.vue
@@ -1,41 +1,59 @@
 <template>
-<v-container fluid>
-  <v-row>
-    <v-col cols="4">
-      <v-btn>Principal</v-btn>
-    </v-col>
-  </v-row>
-  <v-row justify="center">
-    <v-col cols="12" md="6">
-      <v-card class="mx-auto">
-        <v-card-title>
-          <h3>Login</h3>
-        </v-card-title>
-        <v-card-text>
-          <v-text-field label="Usuario" outlined rules="required"></v-text-field>
-          <v-text-field 
-            :type="!show ? 'password' : 'text'"
-            :append-icon="!show ? 'mdi-eye' : 'mdi-eye-off'"
-            label="Contraseña"
-            outlined
-            @click:append="show = !show"
-          >
-          </v-text-field>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn>Aceptar</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
-</v-container>
+  <v-app>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="4">
+          <v-btn>Principal</v-btn>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="12" md="6">
+          <v-form lazy-validation>
+            <v-card>
+              <v-card-title>
+                <h3>Login</h3>
+              </v-card-title>
+              <v-text-field 
+                v-model="email"
+                :rules="[rules.required, rules.email]"
+                label="Usuario"
+                outlined
+              ></v-text-field>
+              <v-text-field 
+                v-model="password"
+                :type="!show ? 'password' : 'text'"
+                :append-icon="!show ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[rules.required, rules.min]"
+                label="Contraseña"
+                outlined
+                @click:append="show = !show"
+              ></v-text-field>
+              <v-card-actions>
+                <v-btn>Aceptar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      show: false
+      email: '',
+      password: '',
+      show: false,
+      rules: {
+        required: value => !!value || 'Requerido.',
+        min: v => v.length >= 8 || 'Minimo 8 caracteres.',
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Ingrese un E-mail válido.'
+        }
+      }
     }
   }
 }
