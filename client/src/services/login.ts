7f54f4ce9ecbdc130e166a949478bde4e6a0222a
AngelJGR <ajguerreror@gmail.com>
2020-12-10 17:33:45 -0400

    (client): Cambia nombre de servicio a login

--- client/src/services/login.ts
+++ client/src/services/login.ts
@@ -4,5 +4,8 @@ const baseUrl = 'http://localhost:4000'
 export default {
     validateUser: (user: string) => {
         return Axios.get(`${baseUrl}/validateUser?user=${user}`)
+    },
+    registerUser: (fullName: string, user: string, password: string) => {
+        return Axios.post(`${baseUrl}/registerUser`, {fullName, user, password})
     }
 }
