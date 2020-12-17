62b52905ec1d9a60b37038b9fbad45d8b5e3b007
AngelJGR <ajguerreror@gmail.com>
2020-12-17 17:16:05 -0400

    (client): Añade componente padre y add para administrar información

---
+++ client/src/views/app-layout.vue
@@ -0,0 +1,10 @@
+<template>
+<v-main class="grey lighten-3">
+	<v-container fluid fill-height :class="{'pa-0': $vuetify.breakpoint.xs}">
+		<v-layout :class="{'pa-0': $vuetify.breakpoint.xs}" justify-center>
+				<router-view />
+		</v-layout>
+	</v-container>
+</v-main>
+	
+</template>
