c9ce457cf666ae242caccc4136521b94b055624d
AngelJGR <ajguerreror@gmail.com>
2020-12-17 17:14:55 -0400

    (client): Jerarquiza rutas para los componentes

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,26 +2,44 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Login from '../views/options/login.vue'
 import Register from '../views/options/register.vue'
-import Home from '../views/Home.vue'
+import AppLayout from '../views/app-layout.vue'
+import Add from '../views/add.vue'
+import App from '../App.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
+  /* {
     path: '/',
     name: 'Home',
     component: Home
+  }, */
+  {
+    path: '/auth',
+    component: App,
+    children: [
+      {
+        path: 'login',
+        name: 'Login',
+        component: Login
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: Register
+      }
+    ]
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }
-  ,
-  {
-    path: '/Register',
-    name: 'Register',
-    component: Register
+    path: '/',
+    component: AppLayout,
+    children: [
+      {
+        path: '/add',
+        name: 'Add',
+        component: Add
+      }
+    ]
   }
 ]
 
