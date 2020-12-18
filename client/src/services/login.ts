e4421e44fab212149980ad3bba07b172ec1d4b3e
AngelJGR <ajguerreror@gmail.com>
2020-12-18 16:33:56 -0400

    (client): Identa tabulaciones a 2 espacios

--- client/src/services/login.ts
+++ client/src/services/login.ts
@@ -2,13 +2,13 @@ import Axios from 'axios'
 
 const baseUrl = 'http://localhost:4000'
 export default {
-    validateUser: (user: string) => {
-        return Axios.get(`${baseUrl}/validateUser?user=${user}`)
-    },
-    registerUser: (fullname: string, user: string, password: string) => {
-        return Axios.post(`${baseUrl}/registerUser`, {fullname, user, password})
-    },
-    login: (user: string, password: string) => {
-        return Axios.post(`${baseUrl}/login`, {user, password})
-    }
+  validateUser: (user: string) => {
+    return Axios.get(`${baseUrl}/validateUser?user=${user}`)
+  },
+  registerUser: (fullname: string, user: string, password: string) => {
+    return Axios.post(`${baseUrl}/registerUser`, {fullname, user, password})
+  },
+  login: (user: string, password: string) => {
+    return Axios.post(`${baseUrl}/login`, {user, password})
+  }
 }
