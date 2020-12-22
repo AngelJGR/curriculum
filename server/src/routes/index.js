503a66bbbf66fb17730bfc6206c934dd49fccc05
AngelJGR <ajguerreror@gmail.com>
2020-12-22 15:43:46 -0400

    (server): Cambia denominaciones de campos y tablas de los scripts sql

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,31 +5,30 @@ const pool = require("../database");
 
 router.get("/", async (req, res) => {
 	//Consulta de los datos personales
-	const personas = await pool.query("SELECT * FROM persona");
-	const persona = personas[0];
-	const { id } = persona;
-	//Consulta de los titulos
-	const persona_titulos = await pool.query("SELECT b.id, b.fullname, c.descripcion, \
-	c.id_tipo_titulo, d.nombre FROM persona_titulo AS a, persona AS b, titulos AS c, \
-	casa_de_estudio AS d WHERE a.id_persona = b.id AND a.id_titulo = c.id \
-	AND a.id_casa_de_estudio = d.id AND id_tipo_titulo = 1 AND b.id = ?", [id]);
-	//Consulta de los cursos
-	const persona_cursos = await pool.query("SELECT b.id, b.fullname, c.descripcion, \
-	c.id_tipo_titulo, d.nombre FROM persona_titulo AS a, persona AS b, titulos AS c, \
-	casa_de_estudio AS d WHERE a.id_persona = b.id AND a.id_titulo = c.id \
-	AND a.id_casa_de_estudio = d.id AND id_tipo_titulo = 2 AND b.id = ?", [id]);
-	//Consulta de las habilidades
-	const habilidades = await pool.query("SELECT a.descripcion, b.calificacion \
-	FROM habilidades AS a, habilidades_personas AS b, persona AS c \
-	WHERE a.id = b.id_habilidad AND c.id = b.id_persona AND c.id = ?", [id]);
-	//Experiencia Laboral
-	const experiencia = await pool.query("SELECT a.titulo, a.descripcion, c.nombre \
-	FROM experiencia_laboral AS a, persona AS b, instituto AS c \
-	WHERE a.id_persona = b.id AND a.id_instituto = c.id AND b.id = ?", [id]);
+	
+	const person = await pool.query("SELECT * FROM person WHERE username = ?", req);
+	// const persona = personas[0];
+	const { id } = person;
+	//Consulta de los titulos A college_degree_person, B person, C college_degree, D college
+	const college_degree_person = await pool.query("SELECT b.id, b.fullname, c.description, \
+	c.id_college_degree_type, d.name FROM college_degree_person AS a, person AS b, college_degree AS c, \
+	college AS d WHERE a.id_person = b.id AND a.id_college_degree = c.id \
+	AND a.id_college = d.id AND c.id_college_degree_type = 1 AND b.id = ?", [id]);
+	//Consulta de los cursos A college_degree_person, B person, C college_degree, D college
+	const courses_person = await pool.query("SELECT b.id, b.fullname, c.description, \
+	c.id_college_degree_type, d.name FROM college_degree_person AS a, person AS b, college_degree AS c, \
+	college AS d WHERE a.id_person = b.id AND a.id_college_degree = c.id \
+	AND a.id_college = d.id AND id_college_degree_type = 2 AND b.id = ?", [id]);
+	//Consulta de las habilidades A skills, B person_skills C person
+	const skills = await pool.query("SELECT a.description, b.score \
+	FROM skills AS a, person_skills AS b, person AS c \
+	WHERE a.id = b.id_skill AND c.id = b.id_person AND c.id = ?", [id]);
+	//Experiencia Laboral A work_experience, B person, C organization
+	const experience = await pool.query("SELECT a.area, a.description, c.name \
+	FROM work_experience AS a, person AS b, organization AS c \
+	WHERE a.id_person = b.id AND a.id_organization = c.id AND b.id = ?", [id]);
 
-	res.render("./", { persona, persona_titulos, persona_cursos, habilidades, experiencia });
+	res.json({ person, college_degree_person, courses_person, skills, experience });
 });
-
-
 
 module.exports = router;
