2c49b6cd677aac1cab729a62d239f5f1547280fc
AngelJGR <ajguerreror@gmail.com>
2021-01-06 17:13:43 -0400

    (server): Recibe datos en endpoint updatePerson para guardarlo en la base de datos

--- server/src/routes/person.js
+++ server/src/routes/person.js
@@ -9,17 +9,24 @@ const { isLoggedIn } = require("../lib/auth");
 
 router.get("/getPerson/:id", isLoggedIn, async (req, res) => {
 	const { id } = req.params;
-	console.log('Llegue. Id:', id)
 	const person = await pool.query("SELECT * FROM person WHERE id = ?", [id]);
 	res.json({ person })
 });
 
-router.post("/basico/:id", isLoggedIn, async (req, res) => {
-	const { id } = req.params;
-	const datos = req.body;
-	await pool.query("UPDATE persona SET ? WHERE id = ?", [datos, id]);
-	req.flash("success", "Datos Actualizados");
-	res.redirect("/update/basico/" + [id]);
+router.post("/updatePerson/:idUser", isLoggedIn, async (req, res) => {
+	const { idUser } = req.params;
+	const data = req.body.data;
+	try {
+		const result = await pool.query("SELECT * FROM person WHERE id_user = ?", [idUser]);
+		if (result.length === 0) {
+			await pool.query(`INSERT INTO person SET fullname = '${data.fullname}', ci = ${data.ci}, address = '${data.address}', phone = '${data.phone}', twitter = '${data.twitter}', email = '${data.email}', about = '${data.about}', id_user = ${idUser}`);
+		} else {
+			await pool.query(`UPDATE person SET fullname = '${data.fullname}', ci = ${data.ci}, address = '${data.address}', phone = '${data.phone}', twitter = '${data.twitter}', email = '${data.email}', about = '${data.about}' WHERE id_user = ${idUser}`);
+		}
+		res.send({success: true});
+	} catch(err) {
+		res.send({success: false, error: err});
+	}
 });
 
 module.exports = router;
