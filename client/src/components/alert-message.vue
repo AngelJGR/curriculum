0d088e7aa1a7324a202e7a18aa8fcd14348a0f0e
AngelJGR <ajguerreror@gmail.com>
2021-01-07 18:18:20 -0400

    (client): Añade snackbar de alertas como componente para ser reutilizado en otros componentes

---
+++ client/src/components/alert-message.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-snackbar
+    v-model="show"
+    :timeout="5000"
+    top
+    :color="color"
+    :message="message"
+  >
+    <b>{{ message }}</b>
+
+    <template v-slot:action="{ attrs }">
+      <v-btn
+        icon
+        color="dark"
+        v-bind="attrs"
+        @click="show = false"
+      >
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    message: String,
+    snackbar: Boolean,
+    color: String,
+  },
+  data() {
+    return {
+      show: false
+    }
+  },
+  created() {
+    this.show = this.snackbar
+  }
+})
+</script>
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,7 +1,9 @@
 import NonContent from './non-content.vue'
+import AlertMessage from './alert-message.vue'
 
 const components = [
-  {key: 'NonContent', component: NonContent}
+  {key: 'NonContent', component: NonContent},
+  {key: 'Message', component: AlertMessage}
 ]
 
 export default (vue: any) => {
