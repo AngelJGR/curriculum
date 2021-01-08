8ba24501ad2451522b5f156a14a177cc67d06a37
AngelJGR <ajguerreror@gmail.com>
2021-01-08 11:50:25 -0400

    (client): Agrega propiedad updatePerson para enviar datos al servidor para actualizar

--- client/src/services/person.ts
+++ client/src/services/person.ts
@@ -4,5 +4,8 @@ const prefix = 'person'
 export default {
   getPerson: (userId: number) => {
     return Axios.get(`${prefix}/getPerson/${userId}`)
+  },
+  updatePerson: (id: number, data: Record<string, any>) => {
+    return Axios.post(`${prefix}/updatePerson/${id}`, { data })
   }
 }
