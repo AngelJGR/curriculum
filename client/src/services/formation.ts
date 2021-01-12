1f01f07453149d98109ed8cb727e53ca570c9716
AngelJGR <ajguerreror@gmail.com>
2021-01-11 21:10:14 -0400

    (client): Añade servico setCollegeDegree para guardar formación del usuario

--- client/src/services/formation.ts
+++ client/src/services/formation.ts
@@ -6,10 +6,13 @@ export default {
   getFormation: (userId: number) => {
     return Axios.get(`${prefix}/getFormation/${userId}`)
   },
-  getColleges: () => {
+  /* getColleges: () => {
     return Axios.get(`${prefix}/getColleges`)
-  },
+  }, */
   getCollegeDregrees: (text: string) => {
     return Axios.post(`${prefix}/getCollegeDregrees`, { text })
+  },
+  setCollegeDegree: (idPerson: number, idCollegeDegree: number, idCollege: number) => {
+    return Axios.post(`${prefix}/setCollegeDegree`, { idPerson, idCollegeDegree, idCollege })
   }
 }
