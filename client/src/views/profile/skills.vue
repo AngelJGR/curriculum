9cdb55a514d33ca4c5665bb0ca40adb204153809
AngelJGR <ajguerreror@gmail.com>
2021-01-14 19:17:54 -0400

    (client): Agrega método para setear habilidad a usuario, obtiene habilidad en el campo de busqueda que no esté seteado al usuario

--- client/src/views/profile/skills.vue
+++ client/src/views/profile/skills.vue
@@ -2,48 +2,65 @@
   <v-container>
     <h1>Habilidades</h1>
     <non-content v-if="isEmpty"></non-content>
+    <v-card v-else>
+      <v-row v-for="(item, index) in skillsPerson" :key="item.id">
+        <v-col cols="12" sm="6">
+          {{index}} - {{item.description}}
+        </v-col>
+        <v-col cols="12" sm="4">
+          {{item.score}}
+        </v-col>
+        <v-col cols="12" sm="2">
+          <v-btn icon color="success">
+            <v-icon>mdi-refresh-circle</v-icon>
+          </v-btn>
+          <v-btn icon color="error">
+            <v-icon>mdi-delete-circle</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
 
     <v-divider></v-divider>
     <v-form lazy-validation ref="form" @submit.prevent="setSkill">
       <v-card>
         <v-row justify="center">
           <v-col cols="6">
-            <v-text-field
+            <v-combobox
+              v-model="skill"
+              :items="skills"
               label="Habilidad"
-              placeholder="Describa habilidad"
-              v-model="skill.description"
+              item-text="description"
+              placeholder="Ingrese habilidad"
               :rules="[rules.required]"
-            ></v-text-field>
-            <!-- <v-autocomplete
-              label="Seleccione una carrera"
-              v-model="collegeDegree"
-              :items="collegeDegrees"
-              :item-text="(obj) => obj.description"
-              :item-value="(obj) => obj.id"
-              return-object
+              :searchInput.sync="search"
               clearable
-              :rules="[rules.required]"
-            ></v-autocomplete> -->
+            >
+              <template v-slot:no-data>
+                <v-list-item v-if="!isSearching">
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-progress-circular
+                  v-else
+                  indeterminate
+                  color="primary"
+                ></v-progress-circular>
+              </template>
+            </v-combobox>
           </v-col>
           <v-col cols="4">
             <v-slider
-              v-model="skill.score"
+              v-model="score"
               color="red darken-4"
               min="1"
               max="100"
               label="porcentaje"
               :rules="[rules.required]"
             ></v-slider>
-            <!-- <v-autocomplete
-              label="Seleccione un instituto"
-              v-model="college"
-              :items="colleges"
-              item-text="name"
-              item-value="id"
-              return-object
-              clearable
-              :rules="[rules.required]"
-            ></v-autocomplete> -->
           </v-col>
           <v-col cols="2">
             <v-btn small type="submit" color="success">Agregar</v-btn>
@@ -64,11 +81,12 @@ export default Vue.extend({
   data() {
     return {
       skills: [],
-      skill: {
-        id: null,
-        description: null,
-        score: null
-      },
+      skillsPerson: [],
+      skillPerson: null,
+      skill: null,
+      score: null,
+      search: null,
+      isSearching: false,
       isEmpty: false,
       rules: {
         required: value => !!value || 'Requerido.',
@@ -76,26 +94,41 @@ export default Vue.extend({
     }
   },
   methods: {
-    getSkills() {
-      skills.getSkills(1)
+    getSkillsPerson() {
+      skills.getSkillsPerson(1) // EDITAR
         .then((res) => {
-          if (res.data.skills.length === 0) {
+          if (res.data.skillsPerson.length === 0) {
             this.isEmpty = true
           }
+          this.skillsPerson = res.data.skillsPerson
+          console.log(res)
+        })
+    },
+    getSkills(val) {
+      this.isSearching = true
+      skills.getSkills(1, val) //EDITAR
+        .then((res) => {
+          this.skills = res.data.skills
+          this.isSearching = false
         })
     },
     setSkill() {
-      if (this.$refs.form.validate()) {
-        console.log(this.skill)
-        skills.setSkill()
+      if (this.$refs.form.validate() && this.skill.description === this.search) {
+        skills.setSkill(1, this.skill.id, this.score) // EDITAR
           .then((res) => {
             console.log(res)
+            this.skillsPerson.push(res.data.skill)
           })
       }
     }
   },
+  watch: {
+    search (val) {
+      this.getSkills(val)
+    }
+  },
   created() {
-    this.getSkills()
+    this.getSkillsPerson()
   }
 })
 </script>
