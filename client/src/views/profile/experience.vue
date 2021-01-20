abb8201fc420f98e14bfe96393c0c50dea6d43bb
AngelJGR <ajguerreror@gmail.com>
2021-01-20 17:03:05 -0400

    (client): Añade alert-message para mostrar error en caso de existir al mostrar datos al cargar componente

--- client/src/views/profile/experience.vue
+++ client/src/views/profile/experience.vue
@@ -1,5 +1,45 @@
 <template>
   <v-container>
     <h1>Experiencia</h1>
+    <alert-message
+      :snackbar="snackbar"
+      top
+      :color="color"
+      :message="message"
+    />
   </v-container>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import experience from '../../services/experience'
+export default Vue.extend({
+  data () {
+    return {
+      snackbar: false,
+      color: '',
+      message: '',
+    }
+  },
+  methods: {
+    getExperience (): void {
+      this.snackbar = false
+      experience.getExperience(1) // EDITAR
+        .then((res) => {
+          console.log(res)
+          if (res.data.success) {
+            // this.color = 'success'
+            // this.message = 'Registros obtenidos'
+          } else {
+            this.snackbar = true
+            this.color = 'error'
+            this.message = 'Error al obtener los registros'
+          }
+        })
+    }
+  },
+  created() {
+    this.getExperience()
+  }
+})
+</script>
