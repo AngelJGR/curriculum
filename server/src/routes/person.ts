860d9c84e9d26ed9bb0c31bf8bc52c0da2afd009
AngelJGR <ajguerreror@gmail.com>
2021-01-25 16:07:11 -0400

    (server): Importa el controlador Person y llama los métodos de acuerdo al endpoint requerido

--- server/src/routes/person.ts
+++ server/src/routes/person.ts
@@ -1,39 +1,14 @@
 import { Router } from 'express'
-import { Request } from 'express'
-import { Response } from 'express'
-/* const express = require("express");
-const router = express.Router(); */
 
-import { pool } from '../database'
-
-// const pool = require("../database");
-
-import isLoggedIn from'../lib/auth'
+import PersonController from '../controllers/person'
 
 /** RUTAS PARA BÁSICO **/
 export default () => {
+	const PersonRouter = Router()
 
-	Router().get("/getPerson/:id", async (req: Request, res: Response) => {
-		const { id } = req.params;
-		const person = await pool.query("SELECT * FROM person WHERE id = ?", [id]);
-		res.json({ person })
-	})
+	PersonRouter.get("/getPerson/:id", PersonController.getPerson)
 	
-	Router().post("/updatePerson/:idUser", async (req: Request, res: Response) => {
-		const { idUser } = req.params;
-		const data = req.body.data;
-		try {
-			const result = await pool.query("SELECT * FROM person WHERE id_user = ?", [idUser]);
-			if (result.length === 0) {
-				await pool.query(`INSERT INTO person SET fullname = '${data.fullname}', ci = ${data.ci}, address = '${data.address}', phone = '${data.phone}', twitter = '${data.twitter}', email = '${data.email}', about = '${data.about}', id_user = ${idUser}`);
-			} else {
-				await pool.query(`UPDATE person SET fullname = '${data.fullname}', ci = ${data.ci}, address = '${data.address}', phone = '${data.phone}', twitter = '${data.twitter}', email = '${data.email}', about = '${data.about}' WHERE id_user = ${idUser}`);
-			}
-			res.send({success: true});
-		} catch(err) {
-			res.send({success: false, error: err});
-		}
-	})
-}
+	PersonRouter.post("/updatePerson/:idUser", PersonController.updatePerson)
 
-// module.exports = Router;
+	return PersonRouter
+}
