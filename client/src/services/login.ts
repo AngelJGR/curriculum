060b823b95ac7ad8ada3a214ee5b55ed2019ab24
AngelJGR <ajguerreror@gmail.com>
2021-01-26 13:04:11 -0400

    (client): Añade prefijo auth para llamar endpoint

--- client/src/services/login.ts
+++ client/src/services/login.ts
@@ -1,13 +1,14 @@
 import Axios from 'axios'
+const prefix = 'auth'
 
 export default {
   validateUser: (username: string) => {
-    return Axios.get(`validateUser/${username}`)
+    return Axios.get(`${prefix}/validateUser/${username}`)
   },
   registerUser: (fullname: string, user: string, password: string) => {
-    return Axios.post(`registerUser`, {fullname, user, password})
+    return Axios.post(`${prefix}/registerUser`, {fullname, user, password})
   },
   login: (user: string, password: string) => {
-    return Axios.post(`login`, {user, password})
+    return Axios.post(`${prefix}/login`, {user, password})
   }
 }
