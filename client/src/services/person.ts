9a5fad361878a8bf2216c6bfae2bb08b764b0c42
AngelJGR <ajguerreror@gmail.com>
2021-03-31 10:40:06 -0400

    Define tipo de dato de objeto

--- client/src/services/person.ts
+++ client/src/services/person.ts
@@ -1,11 +1,13 @@
 import Axios from 'axios'
+import { Person } from '@/interfaces/person'
+
 const prefix = 'person'
 
 export default {
   getPerson: () => {
     return Axios.get(`${prefix}/getPerson`)
   },
-  updatePerson: (data: Record<string, any>) => {
+  updatePerson: (data: Person) => {
     return Axios.post(`${prefix}/updatePerson`, { data })
   }
 }
