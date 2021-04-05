b38c529e70b2bdf1aa88dea449a7fd384886ac11
AngelJGR <ajguerreror@gmail.com>
2021-04-05 15:09:40 -0400

    Guarda objeto user en localstorage y los usa en el baseState

--- client/src/store/modules/session.ts
+++ client/src/store/modules/session.ts
@@ -1,16 +1,21 @@
 import login from '../../services/login'
 import ICommit from '../../interfaces/commit'
+import SessionState from '@/interfaces/session'
 
-const baseState = {
-  user: {},
-  token: ''
+const TOKEN_KEY = 'resume-token'
+const USER_KEY = 'resume-user'
+
+const baseState: SessionState = {
+  user: JSON.parse(localStorage.getItem(USER_KEY) || '{}'),
+  token: (localStorage.getItem(TOKEN_KEY) || '')
 }
 
 const actions = {
-  login ({commit}: ICommit, { username, password}: any) {
+  login ({commit}: ICommit, { username, password }: any) {
     return login.login(username, password)
       .then((session) => {
-        localStorage.setItem('resume-token', session.data.token)
+        localStorage.setItem(TOKEN_KEY, session.data.token)
+        localStorage.setItem(USER_KEY, JSON.stringify(session.data.body))
         if (session.data.success) {
           commit('setSession', session)
         }
@@ -20,7 +25,7 @@ const actions = {
 }
 
 const mutations = {
-  setSession (state: any, session: any) {
+  setSession (state: SessionState, session: any) {
     state.user = session.data.body
     state.token = session.data.token
   },
