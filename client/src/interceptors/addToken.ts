0340ac882cb1210948532df32b142fd9387ffe11
AngelJGR <ajguerreror@gmail.com>
2021-04-05 15:10:38 -0400

    Utiliza el token del store como fuente para los request

--- client/src/interceptors/addToken.ts
+++ client/src/interceptors/addToken.ts
@@ -1,9 +1,10 @@
 
 import { AxiosError, AxiosRequestConfig } from 'axios'
+import store from '../store'
 
 export default {
   success: (config: AxiosRequestConfig): AxiosRequestConfig => {
-    const token: string | null = localStorage.getItem('resume-token')
+    const token: string = store.state.session.token
     if (token != null) {
       config.headers.Authorization = `Bearer ${token}`
     }
