f98702bd3b540c604655fa99e756a08b9e2d6e08
AngelJGR <ajguerreror@gmail.com>
2021-04-12 16:10:41 -0400

    Crea state alert para manejo de alertas en la aplicación

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -3,16 +3,20 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import session from './modules/session'
+import alert from './modules/alert'
+import AlertInterface from '@/interfaces/alert'
 
 Vue.use(Vuex)
 
 const state: {
   session: SessionState;
+  alert?: AlertInterface;
 } = { session: { token: '', user: {} } }
 
 export default new Vuex.Store({
   state,
   modules: {
-    session
+    session,
+    alert
   }
 })
---
+++ client/src/store/modules/alert.ts
@@ -0,0 +1,44 @@
+
+import Alert from '@/interfaces/alert'
+import ICommit from '../../interfaces/commit'
+
+interface MMutations {
+  type: string;
+  message: string;
+}
+
+const baseState: Alert = {
+  show: false,
+  color: '',
+  message: ''
+}
+
+const actions = {
+  success ({ commit }: ICommit, message: string) {
+    commit('alert', { type: 'success', message })
+    return
+  },
+  error ({ commit }: ICommit, message: string) {
+    commit('alert', { type: 'error', message })
+    return
+  },
+  warning ({ commit }: ICommit, message: string) {
+    commit('alert', { type: 'warning', message })
+    return
+  }
+}
+
+const mutations = {
+  alert (state: Alert, { type, message }: MMutations) {
+    state.show = true
+    state.color = type
+    state.message = message
+  }
+}
+
+export default {
+  namespaced: true,
+  state: baseState,
+  actions,
+  mutations
+}
